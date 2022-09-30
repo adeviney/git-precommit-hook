c01b1d4afdb52af62ce8c39ec4911a6b1297ada2
Alexis Deviney <alexis.deviney36@gmail.com>
2022-09-30 16:37:11 -0700

    testing pylint

---
+++ file2.py
@@ -0,0 +1,9 @@
+
+
+print('some key')
+
+print("a really really super duper long crazy line that goes beyond what is recommended at 80 characters and all of that jazz yay yay yay")
+
+
+def some_func():
+    bob = "an unused variable"
